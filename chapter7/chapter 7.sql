7a412616598f9eee3b98257ba19f4562ea225f18
Precious Thandeka Bhembe <bhembepreciouspt@gmail.com>
2024-08-05 21:46:47 +0200

    Chapter updatates

---
+++ chapter7/chapter 7.sql
@@ -0,0 +1,114 @@
+CREATE TABLE natural_key_example (
+    license_id varchar(10) CONSTRAINT license_key PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+
+DROP TABLE natural_key_example;
+
+
+CREATE TABLE natural_key_example (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT license_key PRIMARY KEY (license_id)
+);
+
+SELECT *FROM natural_key_example;
+
+
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO natural_key_example (license_id, first_name, last_name)
+VALUES ('T229901', 'Sam', 'Tracy');
+
+
+CREATE TABLE natural_key_composite_example (
+    student_id varchar(10),
+    school_day date,
+    present boolean,
+    CONSTRAINT student_key PRIMARY KEY (student_id, school_day)
+);
+
+
+SELECT * FROM natural_key_composite_example;
+
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/22/2017', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/23/2017', 'Y');
+
+INSERT INTO natural_key_composite_example (student_id, school_day, present)
+VALUES(775, '1/23/2017', 'N');
+
+
+
+-- Listing 7-5: Declaring a bigserial column as a surrogate key
+
+CREATE TABLE surrogate_key_example (
+    order_number bigserial,
+    product_name varchar(50),
+    order_date date,
+    CONSTRAINT order_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name, order_date)
+VALUES ('Beachball Polish', '2015-03-17'),
+       ('Wrinkle De-Atomizer', '2017-05-22'),
+       ('Flux Capacitor', '1985-10-26');
+
+SELECT * FROM surrogate_key_example;
+
+
+-- Listing 7-6: A foreign key example
+
+CREATE TABLE licenses (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+    registration_id varchar(10),
+    registration_date date,
+    license_id varchar(10) REFERENCES licenses (license_id),
+    CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A203391', '2017/03/17', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '2017/03/17', 'T000001');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
