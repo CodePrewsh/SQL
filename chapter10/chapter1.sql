ae5aa8f011783bbcb3260445567a781cf3b7b472
Precious Thandeka Bhembe <bhembepreciouspt@gmail.com>
2024-08-22 23:44:27 +0200

    updates

--- chapter10/chapter1.sql
+++ chapter10/chapter1.sql
@@ -1,4 +1,4 @@
-
+-- Listing 10-1: Create Census 2011-2015 ACS 5-Year stats table and import data
 CREATE TABLE acs_2011_2015_stats (
     geoid varchar(14) CONSTRAINT geoid_key PRIMARY KEY,
     county varchar(50) NOT NULL,
